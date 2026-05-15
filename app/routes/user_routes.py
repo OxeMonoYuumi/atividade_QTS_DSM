2bb9b0af31efdb23e6d8811c64393de1ece73965
OxeMonoYuumi <pedroxveiga12@gmail.com>
2026-05-15 16:02:38 -0300

    alteração no projeto

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -41,13 +41,12 @@ def get_user(user_id):
 def create():
     data = request.get_json()
 
-    print(request.data)
-    print(request.get_json())
-
     if not data or "name" not in data:
         return jsonify({"error": "name is required"}), 400
 
     user = create_user(data)
+
+    print(user)
 
     if user is None:
         return jsonify({"error": "User already exists"}), 400
