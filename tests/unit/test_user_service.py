a21db8a2c3124cf6033975b8af60b48bd0981e76
OxeMonoYuumi <pedroxveiga12@gmail.com>
2026-05-15 15:00:08 -0300

    Novo test na pasta Unit

---
+++ tests/unit/test_user_service.py
@@ -0,0 +1,11 @@
+def test_should_not_allow_duplicate_users():
+    from app.services import user_service
+
+    user_service.users.clear()
+    user_service.current_id = 1
+
+    user_service.create_user({"name":"Pedro"})
+
+    user = user_service.create_user({"name":"Pedro"})
+
+    assert user is None
