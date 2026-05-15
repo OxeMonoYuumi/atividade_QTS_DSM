a2518e9b353009412f794895cb6fb0c8f4497e10
OxeMonoYuumi <pedroxveiga12@gmail.com>
2026-05-15 14:33:34 -0300

    Criando teste para criaçaõ de 2 usuários

--- tests/e2e/test_user_functional.py
+++ tests/e2e/test_user_functional.py
@@ -33,3 +33,52 @@ def test_create_user_e2e():
     assert any("Pedro" in user.text for user in users)
 
     driver.quit()
+
+def test_create_users_e2e():
+    options = Options()
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get("http://localhost:5000")
+
+    input_name = driver.find_element(By.ID, "name")
+    input_name.send_keys("Marcos")
+
+    button = driver.find_element(By.ID, "submit")
+    button.click()
+
+    from selenium.webdriver.support.ui import WebDriverWait
+
+    wait = WebDriverWait(driver, 5)
+
+    wait.until(
+        lambda d: any("Marcos" in el.text for el in d.find_elements(By.TAG_NAME, "li"))
+    )
+
+    users = driver.find_elements(By.TAG_NAME, "li")
+
+    assert any("Marcos" in user.text for user in users)
+
+    input_name = driver.find_element(By.ID, "name")
+    input_name.send_keys("William")
+
+    button = driver.find_element(By.ID, "submit")
+    button.click()
+
+    from selenium.webdriver.support.ui import WebDriverWait
+
+    wait = WebDriverWait(driver, 5)
+
+    wait.until(
+        lambda d: any("William" in el.text for el in d.find_elements(By.TAG_NAME, "li"))
+    )
+
+    users = driver.find_elements(By.TAG_NAME, "li")
+
+    assert any("William" in user.text for user in users)
+
+    driver.quit()
